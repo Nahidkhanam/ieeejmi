c10c8a7a961099cc5e2f48379d5076aada297e61
Sarfraz <alamsarfraz422@gmail.com>
2022-03-31 18:03:04 +0530

    HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Blogs from "./views/Blogs";
+import Layout from "./components/Layout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="m-0">
-      <h1
-        className="text-5xl"
-      >
-        Hello, world!
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/blogs" element={<Blogs />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Layout/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import IEEEJMI from "../../assets/logo/IEEEJMI.svg";
+
+const route = [
+  {
+    path: "/",
+    text: "Home",
+  },
+  {
+    path: "/team",
+    text: "Team",
+  },
+  {
+    path: "/blogs",
+    text: "Blogs",
+  },
+  {
+    path: "/events",
+    text: "Events",
+  },
+];
+
+function Navbar() {
+  return (
+    <div className="flex pl-10 pt-9 font-body absolute">
+      <img className="w-32" src={IEEEJMI} alt="IEEE JMI" />
+
+      <div className="flex gap-4 ml-7">
+        {route.map((item, index) => (
+          <Link to={item.path} className="self-center" key={index}>
+            <span>{item.text}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
