8f4e12ac3cd13379305b9f13c6b516f47f57f8e5
Sarfraz <alamsarfraz422@gmail.com>
2022-04-09 00:15:55 +0530

    Hover Effects and Svg fixes

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Landing from "./modules/Landing";
-import AboutUs from "./modules/AboutUs";
+import AboutUs from "./modules/Aboutus";
 import SubBranches from "./modules/SubBranches";
-import ContactUs from "./modules/ContactUs";
+import ContactUs from "./modules/Contactus";
 function Home() {
   return (
     <React.Fragment>
--- src/views/Home/modules/SubBranches.jsx
+++ src/views/Home/modules/SubBranches.jsx
@@ -4,23 +4,45 @@ import Pels from "../../../assets/images/home/sub_branches_pels.png";
 import Wie from "../../../assets/images/home/sub_branches_wie.png";
 import Pes from "../../../assets/images/home/sub_branches_pes.png";
 import ButtonLogo from "../../../assets/images/home/sub_branches_button_logo.png";
-import HeadLeft from "../../../assets/images/home/sub_branches_head_logo_left.png";
-import HeadRight from "../../../assets/images/home/sub_branches_head_logo_right.png";
+import HeadLeft from "../../../assets/images/home/sub_branches_head_logo_left.svg";
+import HeadRight from "../../../assets/images/home/sub_branches_head_logo_right.svg";
 
 function SubBranches() {
   return (
-    <div className="min-h-screen flex flex-col justify-center justify-between">
-    <h3 className="flex justify-center text-4xl font-bold text-center mt-8"><img className="mx-1" src={HeadLeft} alt="" /> Sub Branches <img className="mx-1" src={HeadRight} alt="" /></h3>
-    <div className="shrink m-auto max-w-sm grid grid-cols-2 gap-8">
-      <div className="p-2 flex flex-col justify-between">
-        <img src={Ieeecs} alt="ieeecs" />
-        <button className="bg-black max-w-28 mx-auto shrink text-white text-xs rounded-full px-4 py-1"><a className="flex justify-center" href="https://cs.ieeejmi.org/">cs.ieeejmi.org <img className="mx-1 w-3 mt-0.5 h-3" src={ButtonLogo} alt="" /></a></button>
+    <div className="min-h-screen  flex flex-col font-body justify-between">
+      <h3 className="flex justify-center text-4xl font-bold text-center mt-8">
+        <img className="mx-2 rotate-12" src={HeadLeft} alt="" /> Sub Branches{" "}
+        <img className="mx-2" src={HeadRight} alt="" />
+      </h3>
+      <div className="shrink m-auto max-w-sm grid grid-cols-2 gap-10">
+        <div className="p-2  flex flex-col justify-between">
+          <img
+            src={Ieeecs}
+            alt="ieeecs"
+            className="hover:scale-110 transition-all"
+          />
+          <button className="bg-black max-w-28 mx-auto shrink text-white text-xs rounded-full px-4 py-1">
+            <a className="flex justify-center" href="https://cs.ieeejmi.org/">
+              cs.ieeejmi.org{" "}
+              <img className="mx-1 w-3 mt-0.5 h-3 " src={ButtonLogo} alt="" />
+            </a>
+          </button>
+        </div>
+        <div>
+          <img
+            src={Pels}
+            alt="pels"
+            className="hover:scale-110 transition-all"
+          />
+        </div>
+        <div>
+          <img src={Wie} alt="wie" className="hover:scale-110 transition-all" />
+        </div>
+        <div>
+          <img src={Pes} alt="pes" className="hover:scale-110 transition-all" />
+        </div>
       </div>
-      <img src={Pels} alt="pels" />
-      <img src={Wie} alt="wie" />
-      <img src={Pes} alt="pes" />
     </div>
-  </div>
   );
 }
 
