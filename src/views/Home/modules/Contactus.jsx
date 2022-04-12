049d9c11f0005063ee5d6588ff7e42b95aea0957
ShahanOne <90277877+ShahanOne@users.noreply.github.com>
2022-04-13 00:51:54 +0530

    Contact Page fix

--- src/views/Home/modules/Contactus.jsx
+++ src/views/Home/modules/Contactus.jsx
@@ -5,17 +5,19 @@ import "./ContactUs.css"
 
 function ContactUs() {
   return <div className="contact-div">
+  <br/>
+  <div className="contents-lol">
   <p className="contact-head">Contact Us</p>
-  <div className="grid grid-cols-2 gap-2">
+  <div className="grid grid-cols-2 gap-3">
      <div className="form-div">
       <form method="POST"> 
-         <input type="text" className="contact-info" placeholder="Your Name"/>
+         <input type="text" className="contact-info" placeholder="Name"/>
          <br/>
-         <input className="contact-info" type="text" placeholder="Your email"/>
+         <input className="contact-info" type="text" placeholder="Email"/>
          <br/>
          <textarea className="contact-info textarea" type="text" placeholder="Message"/>
          <br/>
-         <button type="button" class=" px-6 py-2.5 bg-red-500 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-blue-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out">
+         <button type="button" class="send-btn px-6 py-2.5 bg-red-500 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-blue-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out">
          SEND
         </button>
 
@@ -26,7 +28,7 @@ function ContactUs() {
       <p>Jamia Millia Islamia, Jamia Nagar, New Delhi</p>
     </div>
   </div>
-
+</div>
    </div>;
 }
 
