9c3b9d58d31c079857613cb6bb5e653e22753a3f
Sarfraz <alamsarfraz422@gmail.com>
2022-04-14 10:51:51 +0530

    Shift to tailwind and removing css file

--- src/views/Home/modules/Contactus.jsx
+++ src/views/Home/modules/Contactus.jsx
@@ -1,35 +1,51 @@
 import React from "react";
-import 'tw-elements';
-import "./ContactUs.css"
-
+import "tw-elements";
+import bgMask from "../../../assets/images/home/contactus/whitecorner.svg";
 
 function ContactUs() {
-  return <div className="contact-div">
-  <br/>
-  <div className="contents-lol">
-  <p className="contact-head">Contact Us</p>
-  <div className="grid grid-cols-2 gap-3">
-     <div className="form-div">
-      <form method="POST"> 
-         <input type="text" className="contact-info" placeholder="Name"/>
-         <br/>
-         <input className="contact-info" type="text" placeholder="Email"/>
-         <br/>
-         <textarea className="contact-info textarea" type="text" placeholder="Message"/>
-         <br/>
-         <button type="button" class="send-btn px-6 py-2.5 bg-red-500 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-blue-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out">
-         SEND
-        </button>
-
-      </form>
+  return (
+    <div className="font-body">
+      <img src={bgMask} alt="bgmask" className="absolute" />
+      <div className="bg-black w-full py-28">
+        <p className="text-white ml-32 text-4xl py-8">Contact Us</p>
+        <div className="flex w-full">
+          <div className="w-1/2 flex flex-col items-center">
+            <form method="POST" className="flex flex-col items-start w-2/3">
+              <div className="flex flex-col items-start bg-white w-full p-3 rounded-sm">
+                <input
+                  type="text"
+                  className="py-2 w-full outline-none"
+                  placeholder="Name"
+                />
+                <input
+                  className="py-2 w-full outline-none"
+                  type="text"
+                  placeholder="Email"
+                />
+                <textarea
+                  className="h-64 py-2 w-full outline-none"
+                  type="text"
+                  placeholder="Message"
+                />
+              </div>
+              <button
+                type="button"
+                class="px-6 py-2.5 bg-red-500 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 transition duration-150 ease-in-out my-4"
+              >
+                SEND
+              </button>
+            </form>
+          </div>
+          <div className="w-1/2 text-white">
+            <p className="opacity-70 my-1">ieeejmi@gmail.com</p>
+            <p className="opacity-70 my-1">
+              Jamia Millia Islamia, Jamia Nagar, New Delhi
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="info-div">
-      <p>ieeejmi@gmail.com</p>
-      <p>Jamia Millia Islamia, Jamia Nagar, New Delhi</p>
-    </div>
-  </div>
-</div>
-   </div>;
+  );
 }
 
 export default ContactUs;
